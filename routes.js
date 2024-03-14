0bc178b7d9ca24651a9c8af56fcc4266fe66e745
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-14 17:16:11 -0500

    tst

--- routes.js
+++ routes.js
@@ -2,5 +2,4 @@
 Router.options.ignoreSegment = 1;
 Router.route("/", "./number.html", (route) => {
     route.route(":amount", "./number.html");
-});
-console.log(Router.routes);
+}).start();
--- routes.ts
+++ routes.ts
@@ -2,6 +2,4 @@ Router.options.ignoreSegment = 1;
 
 Router.route("/", "./number.html", (route: Route) => {
     route.route(":amount", "./number.html");
-})
-
-console.log(Router.routes);
+}).start();
