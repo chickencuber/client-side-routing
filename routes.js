23e5af721b80145376811f979fe72bd47c08de1c
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-17 15:38:22 -0500

    test

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 "use strict";
 Router.options.ignoreSegment = 1;
-Router.route("/", "./number.html", (route) => {
-    route.route("/:amount", "./number.html");
+Router.route("/", "/number.html", (route) => {
+    route.route("/:amount", "/");
 }).start();
--- routes.ts
+++ routes.ts
@@ -1,5 +1,5 @@
 Router.options.ignoreSegment = 1;
 
-Router.route("/", "./number.html", (route: Route) => {
-    route.route("/:amount", "./number.html");
+Router.route("/", "/number.html", (route: Route) => {
+    route.route("/:amount", "/");
 }).start();
